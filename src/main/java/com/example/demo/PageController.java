94bb7a13bbe8afe25dbf859d8f0e01d7e3e78ef8
Erickson M Manabat <mogaerickson3104@gmail.com>
2026-06-20 07:01:29 +0800

    Refactor PageController to change root mapping

--- src/main/java/com/example/demo/PageController.java
+++ src/main/java/com/example/demo/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/question")
+    @GetMapping("/")
     public String question() {
         return "question";
     }
